f5d1900637f5be252628e85604b3fff36b6d7819
Rajabhirup <64340974+Rajabhirup@users.noreply.github.com>
2020-11-30 22:55:43 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,19 +1,30 @@
-// Config object to tell Qlik API where to connect.
+
 var config = {
-  host: 'https://zsdemo.us.qlikcloud.com/',
-  isSecure: true,
-  prefix: '/',
-	webIntegrationId: 'TVLol0VNptxE_JBclCwKZuP6f8KAFD_9'
+    host: 'https://zsdemo.us.qlikcloud.com', //for example, 'abc.us.example.com'
+    prefix: '/',
+    isSecure: true,
+    webIntegrationId: 'TVLol0VNptxE_JBclCwKZuP6f8KAFD_9'
 };
+require.config( {
+    baseUrl: ( config.isSecure ? "https://" : "http://" ) + config.host + config.prefix + "resources",
+    webIntegrationId: config.webIntegrationId
+} );			
 
 
-require(['js/qlik'], function(qlik) {
-
-  var app = qlik.openApp('ZS Data Quality - Version 1.0.qvf', config);
+require( ["js/qlik"], function ( qlik ) {
+	qlik.on( "error", function ( error ) {
+		$( '#popupText' ).append( error.message + "<br>" );
+		$( '#popup' ).fadeIn( 1000 );
+	} );
+	$( "#closePopup" ).click( function () {
+		$( '#popup' ).hide();
+	} );
+    //open apps -- inserted here --
+	var app = qlik.openApp('ZS Data Quality - Version 1.0.qvf', config);
 	
 	//get objects -- inserted here --
-app.getObject('Fields','JzPKHF');
-
-});
+	app.getObject('Fields','JzPKHF');
+		
+} );
 
 
